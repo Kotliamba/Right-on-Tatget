6aa89e649be0c180e1f80fdfdfd7337c72aba4ca
Nick <spbkolia@gmail.com>
2022-02-15 17:48:30 +0300

    First round start with ViewDidLoad

--- Right on Tatget/ViewController.swift
+++ Right on Tatget/ViewController.swift
@@ -14,31 +14,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var slider: UISlider!
     
     var number: Int = 0
-    var round: Int = 0
+    var round: Int = 1
     var points: Int = 0
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        number = Int.random(in: 1...50)
+        value.text = "\(number)"
     }
     @IBAction func trySlider(_ sender: UISlider) {
     }
     
 
     @IBAction func buttonPressed(_ sender: UIButton) {
-        if round == 0 {
-            number = Int.random(in: 1...50)
-            value.text = "\(number)"
-            round = 1
-        } else {
-            let numSlider = Int(slider.value.rounded())
-            switch numSlider {
-            case 0..<number: points += 50 - number + numSlider
-            case number+1...50: points += 50 - numSlider + number
-            case number: points += 50
-            default: print("sompfingh wrong, number is \(number), slider number is \(numSlider)")
-            }
+      
+        let numSlider = Int(slider.value.rounded())
+        switch numSlider {
+        case 0..<number: points += 50 - number + numSlider
+        case number+1...50: points += 50 - numSlider + number
+        case number: points += 50
+        default: print("sompfingh wrong, number is \(number), slider number is \(numSlider)")
         }
         if round == 5 {
             let alert = UIAlertController(title: "Game Over", message: "You are won \(points) points", preferredStyle: .alert)
